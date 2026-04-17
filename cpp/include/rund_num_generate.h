8dd14d2ecda9e7c6b88fe5c90ebaff530e4ff875
Kosolapov <givit3324@gmail.com>
2026-04-17 11:26:39 +0300

    Repo init. Implementation of a rand value generator

---
+++ cpp/include/rund_num_generate.h
@@ -0,0 +1,18 @@
+#ifndef RUND_NUM_GENERATE_H
+#define RUND_NUM_GENERATE_H
+
+#include <iostream>
+#include <cmath>
+#include <random>
+
+
+class RNGenerate {
+private:
+    std::random_device rd;
+public:
+    double LengthGenerate(double a_len);
+    double FiGenerate();
+    double CosTettaGenerate(double g);
+};
+
+#endif // !RUND_NUM_GENERATE_H
---
+++ cpp/src/rund_num_generate.cpp
@@ -0,0 +1,35 @@
+#include <rund_num_generate.h>
+#include <numbers>
+
+double RNGenerate::LengthGenerate(double a_len) {
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dist(0, 1);
+    double ksi = dist(gen);
+    double res_len = -std::log(1 - ksi)*a_len;
+    return res_len;
+}
+
+double RNGenerate::FiGenerate() {
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dist(0, 2*std::numbers::pi);
+    double fi = dist(gen);
+    return fi;
+}
+
+double RNGenerate::CosTettaGenerate(double g) {
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dist(0, 1);
+    double ksi = dist(gen);
+    double cos_tetta = 0;
+    if (g > 0)
+    {
+        double g_p2 = g * g;
+        double tFrag = (1 - g_p2) / (1 - g + 2 * g * ksi);
+        cos_tetta = (1 / (2 * g)) * (1 + g_p2 - (tFrag * tFrag));
+    }
+    else if (g == 0)
+    {
+        cos_tetta = 2 * ksi - 1;
+    }
+    return cos_tetta;
+}
